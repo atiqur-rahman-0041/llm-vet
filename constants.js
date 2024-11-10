df136ee2675c83d8a0e6874626bb0587a3687e18
atiqur-rahman-0041 <atiqur.rahman0041@gmail.com>
2024-11-10 00:56:19 -0500

    added constants

---
+++ constants.js
@@ -0,0 +1,6 @@
+export const severityLevels = ['low', 'medium', 'high', 'critical'];
+export const logDirectory = 'logs';
+export const dataDirectory = 'data';
+export const maxAdvisoryPerPage = 100;
+export const defaultAdvirsoryPerPage = 100;
+export const defaultSeverity = 'high';
